8ae598f725f791bd7cb4a43d8038a5e594e6046c
banri <banri.yanahama@gmail.com>
2025-05-24 13:52:55 +0900

    add Finmap and lookup

--- LeanPlayground/Finmap.lean
+++ LeanPlayground/Finmap.lean
@@ -1,2 +1,19 @@
 import Mathlib.Data.Finmap
 
+def m₀ : Finmap (fun _ : ℕ => ℕ) := ∅        -- nothing inside
+
+#check m₀
+
+def m₁ : Finmap (fun _ : ℕ => ℕ) := m₀.insert 1 10   -- 1 ↦ 10
+
+#check m₁
+
+def m₂ : Finmap (fun _ : ℕ => ℕ) := m₁.insert 2 20   -- 2 ↦ 20
+
+#check m₂
+
+#eval m₁.lookup 1
+
+#eval m₁.lookup 2
+
+#eval m₂.lookup 2
